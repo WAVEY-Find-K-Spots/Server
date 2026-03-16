da6767c1eff9021154fecb48791c4ca06cca90e0
si-zero <snm306@naver.com>
2026-03-16 15:07:30 +0900

    feat: 루트 요청 DTO 추가

---
+++ src/main/java/com/Wavey/WaveyService/route/dto/RouteRequest.java
@@ -0,0 +1,15 @@
+package com.Wavey.WaveyService.route.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RouteRequest {
+    private Long user_id;
+    private String title;
+    private String description;
+    private Float progress;
+}
