3b9ac404f03830b9e3e273b0b24ae2c9117da254
si-zero <snm306@naver.com>
2026-03-16 15:58:30 +0900

    feat: 에러 낚아채는 클래스 선언

---
+++ src/main/java/com/Wavey/WaveyService/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.Wavey.WaveyService.global.exception;
+
+import com.Wavey.WaveyService.global.response.CommonResponse;
+import com.Wavey.WaveyService.global.response.ErrorDetail;
+import com.Wavey.WaveyService.global.response.FieldErrorDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<CommonResponse<Void>> handleCustomException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorDetail errorDetail = ErrorDetail.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(new CommonResponse<>(errorCode.getHttpStatus().value(), null, null, errorDetail));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<CommonResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldErrorDetail> fieldErrors = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new FieldErrorDetail(
+                        error.getField(),
+                        error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                        error.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        ErrorDetail errorDetail = ErrorDetail.builder()
+                .code(ErrorCode.COMMON_INVALID_PARAMETER.getCode())
+                .message(ErrorCode.COMMON_INVALID_PARAMETER.getMessage())
+                .errors(fieldErrors)
+                .build();
+
+        return ResponseEntity
+                .status(ErrorCode.COMMON_INVALID_PARAMETER.getHttpStatus())
+                .body(new CommonResponse<>(ErrorCode.COMMON_INVALID_PARAMETER.getHttpStatus().value(), null, null, errorDetail));
+    }
+}
