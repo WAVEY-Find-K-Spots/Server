0bf157039439cfa4c2741db081ddc92d7e4d2b82
si-zero <snm306@naver.com>
2026-04-13 10:58:40 +0900

    feat: 루트 레포지토리 추가

--- src/main/java/com/Wavey/WaveyService/domain/route/repository/RouteRepository.java
+++ src/main/java/com/Wavey/WaveyService/domain/route/repository/RouteRepository.java
@@ -1,9 +1,19 @@
 package com.Wavey.WaveyService.domain.route.repository;
 
 import com.Wavey.WaveyService.domain.route.entity.Route;
+import com.Wavey.WaveyService.domain.route.entity.Visibility;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    List<Route> findByUserId(Long userId);
+
+    List<Route> findByUserIdAndVisibility(Long userId, Visibility visibility);
+
+    Page<Route> findByVisibility(Visibility visibility, Pageable pageable);
+
+    Page<Route> findByVisibilityAndRouteSpots_SpotIdIn(Visibility visibility, List<Long> spotIds, Pageable pageable);
 }
