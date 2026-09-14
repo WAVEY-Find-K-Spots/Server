608cfac6b7da6f2a1f25a7df7c598d5e352f0792
si-zero <snm306@naver.com>
2026-09-14 16:01:40 +0900

    refactor: 프로필 전용 업로드 코드 제거 (UploadService로 대체)

--- src/main/java/com/Wavey/WaveyService/domain/user/dto/PhotoUploadUrlRequest.java
+++
@@ -1,8 +0,0 @@
-package com.Wavey.WaveyService.domain.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record PhotoUploadUrlRequest(
-        @NotBlank String contentType
-) {
-}
