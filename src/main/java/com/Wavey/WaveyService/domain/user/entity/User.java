cc2c664e04b71ece8bd826bb958279a7b4092195
JOO <jucheonsu@gmail.com>
2026-09-14 18:24:31 +0900

    Fix: CI 오류 해결 (User provider_id unique 제약 조건 제거)

--- src/main/java/com/Wavey/WaveyService/domain/user/entity/User.java
+++ src/main/java/com/Wavey/WaveyService/domain/user/entity/User.java
@@ -23,7 +23,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String providerId;
 
     @Column(nullable = false)
